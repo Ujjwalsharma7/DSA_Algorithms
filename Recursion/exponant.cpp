323d164781aed22344aea53558341f9843849eb5
ujjwal <ujjwalsharma1772003@gmail.com>
2023-06-05 22:52:48 +0530

    exponant

---
+++ Recursion/exponant.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int power(int a, int b){
+
+if(b==0){
+    return 1;
+}
+if(b==1)
+{
+    return a;
+}
+int ans = power(a,b/2);
+
+if(b%2==0){
+    return ans*ans;
+}
+else{
+    return a*ans*ans;
+}
+
+
+}
+
+
+int main(){
+int a;
+int b;
+cout<<"Enter a"<<endl;
+cin>>a;
+cout<<"Enter b"<<endl;
+cin>>b;
+
+
+int ans = power(a,b);
+cout<<ans;
+
+
+    return 0;
+}
