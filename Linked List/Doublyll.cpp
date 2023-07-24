f5b9c8cd34fb00258d95921ebce645597c22aa79
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-07-24 17:27:53 +0530

    added getlen, insertatnode func in doubly ll

---
+++ Linked List/Doublyll.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+    public:
+     int data;
+     Node* next;
+     Node* prev;
+
+     //contrucor
+    Node(int d){
+        this ->data = d;
+        this ->next = NULL;
+        this -> prev = NULL;
+    }
+ };
+
+ void print(Node* &head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<<temp -> data <<" ";
+        temp = temp -> next;
+    }
+ }
+
+ int getLength(Node* &head){
+    int len = 0;
+    Node* temp = head;
+    while (temp != NULL)
+    {
+        len++;
+        temp = temp -> next;
+    }
+    return len;
+ }
+
+ void insertAtHeadNode(Node* &head, int d){
+    Node* temp = new Node(d);
+    temp -> next = head;
+    head -> prev =temp;
+    head = temp;
+ }
+
+int main(){
+     Node* node1 = new Node(10);
+     Node* head = node1;
+
+     print(head);
+     cout<<getLength(head)<<endl;;
+     insertAtHeadNode(head, 15);
+     print(head);
+    return 0;
+}
