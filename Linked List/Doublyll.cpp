8bd6ed5b37213e09f047890947ff6291cd4c72f1
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-07-25 21:06:53 +0530

    added insertatposition func

--- Linked List/Doublyll.cpp
+++ Linked List/Doublyll.cpp
@@ -35,6 +35,12 @@ class Node {
  }
 
  void insertAtHeadNode(Node* &head, int d){
+
+   if(head == NULL){
+      Node* temp = new Node(d);
+      head = temp;
+   }
+
     Node* temp = new Node(d);
     temp -> next = head;
     head -> prev =temp;
@@ -42,19 +48,56 @@ class Node {
  }
 
  void inserttTail(Node* &tail, int d){
+
+     if(tail == NULL){
+      Node* temp = new Node(d);
+      tail = temp;
+   }
     Node* temp = new Node(d);
     tail -> next = temp;
     temp -> prev = tail;
     tail = temp;
  }
+ void InsertatPosition(int p, Node* &head, int d, Node* &tail){
+  if(p == 1){
+    insertAtHeadNode(head,d);
+    return;
+  }
+  
+  Node* temp = head;
+  
+  
+  for(int i=1; i<p-1; i++){
+   temp = temp -> next;
+  }
+
+  if(temp -> next == NULL){
+    inserttTail(tail, d);
+    return;
+  }
+
+  Node* u = new Node(d);
+   u -> next = temp -> next;
+   u ->prev = temp;
+   temp -> next -> prev = u;
+   temp -> next = u;
+
+
+}
 
 int main(){
      Node* node1 = new Node(10);
      Node* head = node1;
+     Node* tail = node1;
 
-     print(head);
+   //   print(head);
      cout<<getLength(head)<<endl;;
      insertAtHeadNode(head, 15);
+   //   print(head);
+     insertAtHeadNode(head, 20);
+     insertAtHeadNode(head, 25);
+     inserttTail(tail, 69);
+     InsertatPosition(2, head , 555, tail);
      print(head);
     return 0;
 }
