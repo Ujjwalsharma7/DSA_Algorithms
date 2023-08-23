bc4464dcf43d73c73dd1194d7e76b756d948effc
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-08-23 17:43:04 +0530

    added reverse order traversal

--- Stack/trees/Binarytree.cpp
+++ Stack/trees/Binarytree.cpp
@@ -68,6 +68,40 @@ void levelOrderTraversal(node *root)
         }
     }
 }
+void reverseLevelOrder(node *root)
+{
+    stack<node *> S;
+    queue<node *> Q;
+    Q.push(root);
+
+    // Do something like normal level order traversal order. Following are the
+    // differences with normal level order traversal
+    // 1) Instead of printing a node, we push the node to stack
+    // 2) Right subtree is visited before left subtree
+    while (Q.empty() == false)
+    {
+        /* Dequeue node and make it root */
+        root = Q.front();
+        Q.pop();
+        S.push(root);
+
+        /* Enqueue right child */
+        if (root->right)
+            Q.push(root->right); // NOTE: RIGHT CHILD IS ENQUEUED BEFORE LEFT
+
+        /* Enqueue left child */
+        if (root->left)
+            Q.push(root->left);
+    }
+
+    // Now pop all items from stack one by one and print them
+    while (S.empty() == false)
+    {
+        root = S.top();
+        cout << root->data << " ";
+        S.pop();
+    }
+}
 
 int main()
 {
@@ -75,7 +109,9 @@ int main()
 
     // creating a TREE
     root = buildtree(root);
-    levelOrderTraversal(root);
+    // levelOrderTraversal(root);
+
+    reverseLevelOrder(root);
 
     return 0;
 }
