50f0b292fa2ce69ecf829b07a1c6f7b206f16147
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-08-23 17:40:30 +0530

    added level order traversal

--- Stack/trees/Binarytree.cpp
+++ Stack/trees/Binarytree.cpp
@@ -34,11 +34,48 @@ node *buildtree(node *root)
     root->right = buildtree(root->right);
 }
 
+void levelOrderTraversal(node *root)
+{
+    queue<node *> q;
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty())
+    {
+        node *temp = q.front();
+
+        q.pop();
+
+        if (temp == NULL)
+        {
+            cout << endl;
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        else
+        {
+            cout << temp->data << " ";
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
 int main()
 {
     node *root = NULL;
 
     // creating a TREE
     root = buildtree(root);
+    levelOrderTraversal(root);
+
     return 0;
 }
